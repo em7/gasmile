58af88793e4a4691f9c1790664237a1fd4daaa2d
em7 <mm007.emko@gmail.com>
2024-03-25 18:44:47 +0100

    fontawesome

--- src/layout.lisp
+++ src/layout.lisp
@@ -16,7 +16,9 @@
       (:meta :name "viewport" :content "width=device-width, initial-scale=1")
       (:title "GAS MILES = GA SMILES")
                                         ;(:script :src "https://unpkg.com/htmx.org@1.9.11")
-      (:link :href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" :rel "stylesheet" :integrity"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" :crossorigin "anonymous"))
+      (:link :href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" :rel "stylesheet" :integrity"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" :crossorigin "anonymous")
+      (:link :href "/static/gasmile.css" :rel "stylesheet")
+      (:link :href "/static/fontawesome/css/all.css" :rel "stylesheet"))
      (:body
       :class "container-fluid text-bg-dark p-0"
 
@@ -24,26 +26,31 @@
        :class "d-flex flex-nowrap text-bg-dark"
 
        (:nav
+        :id "sidebar"
         :class "d-flex flex-column flex-shrink-0 p-3 text-bg-dark vh-100"
         :style "width: 280px;"
-        :id "sidebar"
 
         (:div
          :class "d-flex justify-content-between align-items-center"
 
          (:h1 "Hello World")
-         ;; (:button
-         ;;  :class "btn btn-light"
-         ;;  :onclick (:raw (ps (toggle-sidebar)))
-         ;;  (:i
-         ;;   :class "bi bi-list"))
+         (:button
+          :class "btn btn-light"
+          :onclick (:raw (ps (toggle-sidebar)))
+          (:i
+           :class "fas fa-bars"))
          ))
        
 
        (:article
-        :class  "d-flex flex-column text-bg-light vw-100"
+        :id "main-section"
+        :class  "container d-flex flex-column text-bg-light vw-100"
+        
 
-        (:p "Hello World from unfinished content")))
+        (:section
+         :class "content-wrapper"
+
+         (:p "Hello World from unfinished content"))))
 
       (:script :src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" :integrity "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" :crossorigin "anonymous")
 
